e8cdc0a52c413c689747830c8f934cd70cf45179
longhaoteng <lht19941025@163.com>
2022-02-21 16:38:53 +0800

    db pool

--- config/db.go
+++ config/db.go
@@ -12,11 +12,19 @@ type dbConf struct {
 	Host         string `json:"host"`
 	Port         int    `json:"port"`
 	LowThreshold int    `json:"low_threshold"`
+	MaxOpenConns int    `json:"max_open_conns"`
+	MaxIdleConns int    `json:"max_idle_conns"`
 }
 
 func (db *dbConf) init() error {
 	if err := Get(consts.DBConfKey).Scan(db); err != nil {
 		return err
+	}
+	if db.MaxOpenConns == 0 {
+		db.MaxOpenConns = 100
+	}
+	if db.MaxIdleConns == 0 {
+		db.MaxIdleConns = 25
 	}
 	return nil
 }
