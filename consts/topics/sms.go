b6be0ca7397362aea64aa892717c6e7b46cd22bc
longhaoteng <lht19941025@163.com>
2022-05-05 10:37:37 +0800

    :sparkles: 增加短信模板

--- consts/topics/sms.go
+++ consts/topics/sms.go
@@ -3,5 +3,6 @@ package topics
 const (
 	SmsMaiqiVipCreate   = "daydream.sms.maiqi.vip.create"
 	SmsMaiqiVipUpdate   = "daydream.sms.maiqi.vip.update"
+	SmsMaiqiVipUpdateV2 = "daydream.sms.maiqi.vip.update.v2"
 	SmsMaiqiVipBirthday = "daydream.sms.maiqi.vip.birthday"
 )
